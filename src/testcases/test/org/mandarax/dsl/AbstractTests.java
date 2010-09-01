f6984050d81d9ed5a06d3f247d383f901470fe96
jens dietrich <jens dietrich@localhost>
2010-09-02 10:40:39 +1200

    added license/copyright info to classes

--- src/testcases/test/org/mandarax/dsl/AbstractTests.java
+++ src/testcases/test/org/mandarax/dsl/AbstractTests.java
@@ -1,4 +1,16 @@
+/*
+ * Copyright 2010 Jens Dietrich 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the 
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language governing permissions 
+ * and limitations under the License.
+ */
+
 package test.org.mandarax.dsl;
+
 import org.mandarax.dsl.Expression;
 import org.mandarax.dsl.util.ExpressionPrinter;
 import org.mandarax.dsl.util.ExpressionStructurePrinter;
