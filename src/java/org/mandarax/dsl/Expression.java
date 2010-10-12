968d1faab26307d5c26b876c803dbe7f08adcc80
jens dietrich <jens dietrich@localhost>
2010-10-12 15:53:39 +1300

    added more utility methods to expression

--- src/java/org/mandarax/dsl/Expression.java
+++ src/java/org/mandarax/dsl/Expression.java
@@ -14,7 +14,10 @@ package org.mandarax.dsl;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
+
+import com.google.common.base.Predicate;
 
 
 /**
@@ -75,4 +78,68 @@ public abstract class Expression extends ASTNode {
 			return true;
 		}
 	}
+	
+	/**
+	 * Get all expression children of this node.
+	 * @return
+	 */
+	public Collection<Expression> getAllChildren() {
+		Predicate<Object> nullfilter = new Predicate<Object>() {
+			@Override
+			public boolean apply(Object arg0) {
+				return true;
+			}};
+		return getAllChildren(nullfilter);
+	}
+	/**
+	 * Get all expression children of this node matching a filter.
+	 * @return
+	 */
+	public Collection<Expression> getAllChildren(final Predicate<? super Expression> filter) {
+		
+		class VariableCollector extends AbstractASTVisitor {
+			Collection<Expression> children = new LinkedHashSet<Expression>();
+			private boolean add(Expression x) {
+				if (x!=Expression.this && filter.apply(x)) {
+					children.add(x);
+				}
+				return true;
+			}
+			@Override
+			public boolean visit(BinaryExpression x) {return add(x);}
+			@Override
+			public boolean visit(BooleanLiteral x) {return add(x);}
+			@Override
+			public boolean visit(CastExpression x) {return add(x);}
+			@Override
+			public boolean visit(ConditionalExpression x) {return add(x);}
+			@Override
+			public boolean visit(InstanceOfExpression x) {return add(x);}
+			@Override
+			public boolean visit(IntLiteral x) {return add(x);}
+			@Override
+			public boolean visit(MemberAccess x) {return add(x);}
+			@Override
+			public boolean visit(StringLiteral x) {return add(x);}
+			@Override
+			public boolean visit(UnaryExpression x) {return add(x);}
+			@Override
+			public boolean visit(Variable x) {return add(x);}
+			@Override
+			public boolean visit(FunctionInvocation x) {return add(x);}
+			@Override
+			public boolean visit(ConstructorInvocation x) {return add(x);}
+			@Override
+			public boolean visit(NullValue x) {return add(x);}
+
+		}
+		VariableCollector collector = new VariableCollector();
+		this.accept(collector);
+		return collector.children;
+
+		
+	}
+	
+	
+	
 }
