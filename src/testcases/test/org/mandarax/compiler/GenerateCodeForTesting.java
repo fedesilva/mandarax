0061ebe474bee11e8d6196f01e6ea171f8f07acd
jens dietrich <jens dietrich@localhost>
2010-11-01 20:28:15 +1300

    compiler tests that worked before refactoring scheduler are working again CompilerTests4-6

--- src/testcases/test/org/mandarax/compiler/GenerateCodeForTesting.java
+++ src/testcases/test/org/mandarax/compiler/GenerateCodeForTesting.java
@@ -39,12 +39,15 @@ public class GenerateCodeForTesting {
 //			compile(file);
 //		}
 		
-//		compile(new File("src/testcases/test/org/mandarax/compiler/reldef4.rel"));
-//		compile(new File("src/testcases/test/org/mandarax/compiler/reldef5.rel"));
-//		compile(new File("src/testcases/test/org/mandarax/compiler/reldef6.rel"));
-//		compile(new File("src/testcases/test/org/mandarax/compiler/reldef7.rel"));
-//		compile(new File("src/testcases/test/org/mandarax/compiler/reldef8.rel"));
-		compile(new File("src/testcases/test/org/mandarax/compiler/reldef9.rel"));
+		compile(new File("src/testcases/test/org/mandarax/compiler/reldef4.rel"));
+		compile(new File("src/testcases/test/org/mandarax/compiler/reldef5.rel"));
+		compile(new File("src/testcases/test/org/mandarax/compiler/reldef6.rel"));
+		compile(new File("src/testcases/test/org/mandarax/compiler/reldef7.rel"));
+		compile(new File("src/testcases/test/org/mandarax/compiler/reldef8.rel"));
+		// redef9 does not compiler (and is not supposed to), see TestScheduler
+		compile(new File("src/testcases/test/org/mandarax/compiler/reldef10.rel"));
+		compile(new File("src/testcases/test/org/mandarax/compiler/reldef11.rel"));
+		compile(new File("src/testcases/test/org/mandarax/compiler/reldef12.rel"));
 	}
 
 	private static void compile(File file) throws Exception {
